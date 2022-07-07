6746c61f0edf793ae1164ffdf6570fa196644db5
사용자이름 <>
2022-07-07 17:30:13 +0900

    확대 중간완료

--- src/Test.vue
+++ src/Test.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <canvas id="canvas" width="700" height="400" ref = "canvasEl">이 브라우저는 캔버스를 지원하지 않습니다.</canvas>
+  <div class="container">
+    <button @click="enlargeTimeLine()">확대</button>
+    <video class="video" src="gdragon.mp4" ref="videoEl" controls ></video>
+    {{ state.currentFrame }}
+    <div class="frameBox">
+      <div class="frameTimeline" v-bind:style="{left:(state.currentPixel) + 'px'}"></div>
+      <p><canvas class="canvas" width="600" height="40" ref = "canvasEl">이 브라우저는 캔버스를 지원하지 않습니다.</canvas></p>
+    </div>
   </div>
+  
 </template>
 <script lang="ts">
 import { defineComponent, onMounted, reactive, ref } from 'vue'
@@ -11,34 +18,117 @@ export default defineComponent({
   name: 'test-view',
   setup() {    
     const canvasEl = ref<HTMLCanvasElement>();
+    const videoEl = ref<HTMLVideoElement>();
     const canvasClass = ref<DrawCanvas>();
     const state = reactive({
       canvasCtx: null as unknown as CanvasRenderingContext2D,
-      duration: 300,
+      videoDuration:0,
+      currentPixel: 0,
+      ruleFrame: 0,
+      currentFrame: 0,
+      videoFrame: 0,
+      startVideoFrame: 0,
+      myVideoDuration: 0,
+      startPixel: 0,
+      endVideoFrame: 0
     })
 
     const draw = () => {
+      const start = state.startVideoFrame;
+      state.endVideoFrame = Math.ceil(state.videoDuration * state.videoFrame);
+      const end = state.endVideoFrame;
+      state.myVideoDuration = (end - start) / state.videoFrame;
+      drawCanvas(8, start, end);
+    }
+
+    const enlargeTimeLine = () => {
+      const canvas = canvasEl.value;
+      if(canvas)
+      state.canvasCtx.clearRect(0, 0, canvas?.width, canvas?.height)
+      state.startVideoFrame += 50;
+      state.endVideoFrame -= 50;
+      state.myVideoDuration = (state.endVideoFrame - state.startVideoFrame) / state.videoFrame;
+      drawCanvas(8, state.startVideoFrame, state.endVideoFrame);
+      videoCurrentTime()
+    }
+
+    const videoCurrentTime = () => {
+      const video = videoEl.value;
+      const canvas = canvasClass.value;
+
+      if(video && canvas) {
+        video?.play();
+        video.currentTime = state.startVideoFrame / state.videoFrame;
+        const startFrame = Math.ceil(video?.currentTime * state.videoFrame);
+        state.startPixel = canvas.getCurrentPixel(startFrame, state.myVideoDuration);
+        video?.addEventListener('timeupdate', function() {
+          setInterval(function() {
+            state.currentFrame = Math.ceil(video?.currentTime * state.videoFrame);
+            state.currentPixel = canvas.getCurrentPixel(state.currentFrame, state.myVideoDuration) - state.startPixel;
+          })
+        });
+      }
+    }
+
+    const drawVideo = () => {
+      const video = videoEl.value;
+      video?.addEventListener('loadedmetadata', function () {
+        const duration = video?.duration ;
+        state.videoDuration = duration;
+        draw()
+        videoCurrentTime()
+      });
+    }
+
+    const drawCanvas = (ruleFrame: number, start: number, end: number) => {
       const canvas = canvasClass.value;
       if (canvas) {
-        canvas.setRuleFrame(5);
-        canvas.setRuleUnit(25);
-        canvas.draw();
+        canvas.setRuleFrame(ruleFrame);
+        canvas.setRuleUnit(5);
+        canvas.draw(start, end);
+        state.ruleFrame = canvas.ruleFrame;
       }
     }
 
     onMounted(() => {
       state.canvasCtx = canvasEl.value?.getContext('2d') as CanvasRenderingContext2D;
-      canvasClass.value = new DrawCanvas( canvasEl.value, state.canvasCtx, state.duration);
-      
-      draw();
+      if(canvasEl.value) {
+        canvasClass.value = new DrawCanvas( canvasEl.value, state.canvasCtx);
+        state.videoFrame = canvasClass.value?.getVideoFrame();
+      }
+      drawVideo();
     })
 
     return {
       canvasEl,
+      videoEl,
+      state,
+      enlargeTimeLine
     }
-    
-
   }
 
 });
 </script>
+
+<style lang="scss">
+.container {
+  width:100%;
+  height:100vh;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column
+}
+.video {
+  width: 50%;
+}
+.frameTimeline{
+  border-left: 2px solid red;
+  display: inline;
+  height: 50px;
+  position: absolute;
+}
+.frameBox {
+  position: relative;
+}
+</style>
--- src/draw.ts
+++ src/draw.ts
@@ -1,20 +1,36 @@
 class drawCanvas {
   canvas: HTMLCanvasElement | undefined;
   context: CanvasRenderingContext2D | undefined = undefined;
-  duration: number = 0;
   ruleUnit: number = 15;
   ruleFrame: number = 4;
+  ruleWidth: number = 0;
+  shortPositionY: number = 20;
+  longPositionY: number = 30;
+  videoFrame: number = 23.976;
 
-  constructor(canvas: HTMLCanvasElement | undefined, context: CanvasRenderingContext2D, duration: number) {
+  constructor(canvas: HTMLCanvasElement | undefined, context: CanvasRenderingContext2D) {
     this.canvas = canvas;
     this.context = context;
-    this.duration = duration;
   }
-
-  //일정 프레임은 y축을 더 길게 표시
  
   isSatisfiedRepresntText = (x: number):boolean => {
     return x % this.ruleUnit === 0
+  }
+
+  setVideoFrame(frame: number) {
+    this.videoFrame = frame;
+  }
+
+  getVideoFrame() {
+    return this.videoFrame;
+  }
+
+  setShortPositionY(shortY: number) {
+    this.shortPositionY = shortY;
+  }
+
+  setLongPositionY(longY: number) {
+    this.longPositionY = longY;
   }
 
   setRuleUnit(ruleUnit: number) {
@@ -24,25 +40,45 @@ class drawCanvas {
   setRuleFrame(ruleFrame: number) {
     this.ruleFrame = ruleFrame;
   }
-  
 
-  draw() {
+  getCurrentPixel(currentFrame: number, duration: number): number {
+    if(this.canvas) {
+      return currentFrame / (Math.ceil(duration * 23.976) / this.canvas?.width);
+    } else {
+      return 0;
+    }
+  }
+
+  draw(start: number, end: number): void {
     const context = this.context;
-    const duration = this.duration;
     const canvas = this.canvas;
-  
+    const duration = end - start;
     if (context && canvas) {
-      const ruleWidth = canvas.width / duration;
-      context.lineWidth = 0.5;
-      let cnt = 0;
-      /// ?????
-      for (let drawRuleFrame = 0; drawRuleFrame <= duration; drawRuleFrame+=this.ruleFrame) {
-        const rulePositionX = ruleWidth * drawRuleFrame;
-        const drawTextPositionX =  rulePositionX - 5;
-        let linePositionY = 20;
 
-        if (this.isSatisfiedRepresntText(cnt++)) {
-          linePositionY = 30;
+    for(let ruleFrame = 1; ruleFrame < duration; ruleFrame++) {
+      const ruleCnt = duration / ruleFrame;
+      const pixel = canvas.width / ruleCnt;
+
+      if(pixel >= 5 && pixel <= 10) {
+        this.ruleFrame = ruleFrame;
+      }
+    }
+
+    const ruleWidth = canvas.width / duration * this.ruleFrame ;
+
+    this.ruleWidth = ruleWidth;
+    const ruleCnt = canvas.width / ruleWidth;
+
+      context.lineWidth = 0.5;
+
+      for(let cnt = 0; cnt <= ruleCnt; cnt++) {
+        const rulePositionX = ruleWidth * cnt; 
+        const drawTextPositionX =  rulePositionX - 5;
+        let linePositionY = this.shortPositionY;
+        const drawRuleFrame = start + (this.ruleFrame * cnt);
+  
+        if (this.isSatisfiedRepresntText(cnt)) {
+          linePositionY = this.longPositionY;
           context.fillText(String(drawRuleFrame), drawTextPositionX, 40);
         }
 
@@ -51,9 +87,11 @@ class drawCanvas {
         context.moveTo(rulePositionX-0.5, 0);
         context.lineTo(rulePositionX-0.5, linePositionY);
         context.stroke();
+        context.closePath();
       }
     }
   }
 }
 
 export default drawCanvas;
+
